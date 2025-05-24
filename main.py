b18dfa19d5171e28c447623edaacfe1bf64bea95
saymasimu <saymasimu15@gmail.com>
2025-05-24 11:48:25 +0600

    done

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routers import even, matrix, nth_largest, auth, register
 
-app = FastAPI()
+
+app = FastAPI(
+    title="Cloud Computing LAB PAAS",
+    description="ID: 20701026 Name: Sayma Siddiqua Simu"
+)
+
 
 app.include_router(even.router)
 app.include_router(matrix.router)
